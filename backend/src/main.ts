feb47e7b48e22a0502d7334af5fe1e9c458bc50a
Oleksandr Baliubakh <sanya0000010@gmail.com>
2024-03-20 23:17:20 +0200

    make it better

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'https://flower-shop-liard.vercel.app/',
+      origin: 'https://flower111-shop-liard.vercel.app/',
       credentials: true,
     },
   });
