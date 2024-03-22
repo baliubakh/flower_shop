43acebdf7d41944de7eac525d6b31b37b72b9d39
Oleksandr Baliubakh <sanya0000010@gmail.com>
2024-03-22 22:35:59 +0200

    trying to fix cookies

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -31,13 +31,8 @@ export interface ITokens {
 
 const cookieOptions = {
   httpOnly: true,
+  sameSite: 'none' as 'none' | 'lax',
   secure: true,
-  path: '/',
-  maxAge: 24 * 60 * 60 * 1000,
-  domain:
-    process.env.NODE_ENV === 'development'
-      ? 'localhost'
-      : '.flower-shop-liard.vercel.app',
 };
 
 console.log(cookieOptions);
